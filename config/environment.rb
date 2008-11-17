2eb9f08a9ca6928ef99bf66a23edf4504893af0c
Nenad Brcic <neno@neno.ch>
2008-11-17 18:19:02 +0100

    Added Textile Editor Helper (TEH). Defined gem dependencies in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,14 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  config.gem 'fastercsv'
+  config.gem 'prawn'
+  config.gem 'haml'
+  config.gem 'random_data'
+  config.gem 'populator'
+  config.gem 'faker'
+  config.gem 'RedCloth'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
