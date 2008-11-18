835072bde35a3246594d5a8e35de1959fe0090bb
Nenad Brcic <neno@neno.ch>
2008-11-18 23:06:21 +0100

    Replaces default javascript with protoaculous.1.8.1.min and removed unused files

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 function adjustTextarea(textarea, collapsed) {
   var lines = textarea.value.split("\n");
   var count = lines.length;
